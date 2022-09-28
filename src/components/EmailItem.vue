63c6894a5d66f45e3fda88da04363b68c97e4485
Iniubong Obonguko <iniubongobonguko2018@gmail.com>
2022-09-28 10:23:50 +0100

    minor fix

--- src/components/EmailItem.vue
+++ src/components/EmailItem.vue
@@ -38,7 +38,7 @@ export default {
         return this.selectedStatus;
       },
       set(value) {
-        this.$emit("update", value);
+        this.$emit("toggle", value);
       },
     },
   },
